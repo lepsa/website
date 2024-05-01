d84b83b79740951860e85d26a20b5c17ce16cc0a
Owen Harvey <owenlharvey@gmail.com>
2024-05-01 13:23:24 +1000

    Fixing test imports

--- test/Test/Api.hs
+++ test/Test/Api.hs
@@ -12,6 +12,7 @@ import Website.Data.User (User)
 import Website.Data.Entry (Entry)
 import Control.Monad.Reader
 import Test.Db
+import Website.Data.Env
 
 -- Top level API for testing. This is the main api with a set of test specific routes bolted onto the side.
 -- These test APIs only exist in the test suite.
